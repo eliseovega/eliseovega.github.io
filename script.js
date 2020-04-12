fd0da5ec6edf6ae25cf0932c561bc2b1f268e441
eliseovega <vega.eliseo@gmail.com>
2020-04-12 11:23:50 -0500

    form+js
    Desarrollo con Js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function sumar() {
+    var n1;
+    var n2;
+    var suma;
+
+    n1 = Number(document.getElementById("n1").value);
+    n2 = Number(document.getElementById("n2").value);
+    suma=n1+n2;
+    alert("La Suma de: "+n1+" + "+n2+" es : "+suma);   
+}
